0884de18c6cc37a1d0cafd0565d54e4fabcad58b
mrNikGrig <nekitbulding@inbox.ru>
2025-07-18 16:25:34 +0300

    add frontend

--- frontend/main.py
+++ frontend/main.py
@@ -1,10 +1,30 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/chat')
+def chat_page():
+    """Страница для загрузки и анализа чатов"""
+    return render_template('chat.html')
+
+
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     # if request.method == 'POST':
+#     #     # ... существующая логика обработки файла ...
+#     #     return render_template('index.html', 
+#     #                          analysis=analysis_results,
+#     #                          filename=unique_filename)
+    
+#     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
